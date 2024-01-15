12ef76ea466fbe3e705bc2efe44b3a20bb1512eb
sam-echoapps <sam@echoapps360.com>
2024-01-15 21:18:13 +0530

    Dashboard Commit

--- web/js/viewModels/dashboardAdmin.js
+++ web/js/viewModels/dashboardAdmin.js
@@ -13,6 +13,8 @@ function (oj,ko,$, app, ojconverterutils_i18n_1, ArrayDataProvider,  ojknockout_
             self.username = ko.observable('');
             self.CancelBehaviorOpt = ko.observable('icon'); 
             self.StaffDet = ko.observableArray([]);
+            self.blob = ko.observable();
+            self.fileName = ko.observable();
 
             self.menuItems = [
                 {
@@ -101,15 +103,23 @@ function (oj,ko,$, app, ojconverterutils_i18n_1, ArrayDataProvider,  ojknockout_
                     },
                     success: function (data) {
                         console.log(data)
+                        var csvContent = '';
+                        var headers = ['SL.No', 'Name', 'Email','Contact', 'Role', 'Status'];
+                        csvContent += headers.join(',') + '\n';
                         $("#loaderViewPopup").hide();
                          for (var i = 0; i < data[0].length; i++) {
                             if(data[0][i][17] == "Deactive"){
                                 data[0][i][17] = "Inactive"
                             }
-                            self.StaffDet.push({'no': i+1,'id': data[0][i][0],'name' : data[0][i][2] + " " + data[0][i][3], 'email': data[0][i][11],'contact': data[0][i][15] + data[0][i][12],'role': data[0][i][16],'status': data[0][i][17]  });
-
+                            self.StaffDet.push({'no': i+1,'id': data[0][i][0],'name' : data[0][i][2] + " " + data[0][i][3], 'email': data[0][i][11],'contact': data[0][i][15] + data[0][i][12],'role': data[0][i][4],'status': data[0][i][17]  });
+                            var rowData = [i+1, data[0][i][2] + " " +  data[0][i][3],  data[0][i][11],  data[0][i][15]+data[0][i][12],  data[0][i][4],  data[0][i][17]] ;
+                            csvContent += rowData.join(',') + '\n';
                     }
-
+                    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+                    var today = new Date();
+                    var fileName = 'Registered_Users_' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.csv';
+                    self.blob(blob);
+                    self.fileName(fileName);
   
                     self.StaffDet.valueHasMutated();
                     return self; 
@@ -145,11 +155,112 @@ function (oj,ko,$, app, ojconverterutils_i18n_1, ArrayDataProvider,  ojknockout_
                     },
                     success: function (data) {
                         $("#loaderActivePopup").hide();
+                        var csvContent = '';
+                        var headers = ['SL.No', 'Name', 'Email','Contact', 'Role', 'Status'];
+                        csvContent += headers.join(',') + '\n';
                          for (var i = 0; i < data[0].length; i++) {
                             self.StaffDet.push({'no': i+1,'id': data[0][i][0],'name' : data[0][i][2] + " " + data[0][i][3], 'email': data[0][i][11],'contact': data[0][i][15] + data[0][i][12]  });
-
+                            var rowData = [i+1, data[0][i][2] + " " +  data[0][i][3],  data[0][i][11],  data[0][i][15]+ data[0][i][12]] ;
+                            csvContent += rowData.join(',') + '\n';
                     }
-  
+                    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+                    var today = new Date();
+                    var fileName = 'Active_Staff_' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.csv';
+                    self.blob(blob);
+                    self.fileName(fileName);
+                   
+                    self.StaffDet.valueHasMutated();
+                    return self; 
+                }
+                })
+            }
+            
+            self.inactiveStaffPopup = function (event) {
+                getInactiveStaffList();
+                let popup = document.getElementById("activeStaffPopup");
+                popup.open();
+            }
+        
+            self.closeInactiveStaffPopup = function (event) {
+                let popup = document.getElementById("activeStaffPopup");
+                popup.close();
+            }
+            function getInactiveStaffList(){
+                self.StaffDet([]);
+                $("#loaderActivePopup").show();
+                $.ajax({
+                    url: BaseURL + "/jpInactiveStaffDashboardGet",
+                    type: 'GET',
+                    dataType: 'json',
+                    timeout: sessionStorage.getItem("timeInetrval"),
+                    context: self,
+                    error: function (xhr, textStatus, errorThrown) {
+                        if(textStatus == 'timeout' || textStatus == 'error'){
+                            document.querySelector('#TimeoutSup').open();
+                        }
+                    },
+                    success: function (data) {
+                        $("#loaderActivePopup").hide();
+                        var csvContent = '';
+                        var headers = ['SL.No', 'Name', 'Email','Contact', 'Role', 'Status'];
+                        csvContent += headers.join(',') + '\n';
+                         for (var i = 0; i < data[0].length; i++) {
+                            self.StaffDet.push({'no': i+1,'id': data[0][i][0],'name' : data[0][i][2] + " " + data[0][i][3], 'email': data[0][i][11],'contact': data[0][i][15] + data[0][i][12]  });
+                            var rowData = [i+1, data[0][i][2] + " " +  data[0][i][3],  data[0][i][11],  data[0][i][15]+ data[0][i][12]] ;
+                            csvContent += rowData.join(',') + '\n';
+                    }
+                    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+                    var today = new Date();
+                    var fileName = 'Inactive_Staff_' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.csv';
+                    self.blob(blob);
+                    self.fileName(fileName);
+                   
+                    self.StaffDet.valueHasMutated();
+                    return self; 
+                }
+                })
+            }
+
+            self.pendingStaffPopup = function (event) {
+                getPendingStaffList();
+                let popup = document.getElementById("activeStaffPopup");
+                popup.open();
+            }
+        
+            self.closePendingStaffPopup = function (event) {
+                let popup = document.getElementById("activeStaffPopup");
+                popup.close();
+            }
+            function getPendingStaffList(){
+                self.StaffDet([]);
+                $("#loaderActivePopup").show();
+                $.ajax({
+                    url: BaseURL + "/jpPendingStaffDashboardGet",
+                    type: 'GET',
+                    dataType: 'json',
+                    timeout: sessionStorage.getItem("timeInetrval"),
+                    context: self,
+                    error: function (xhr, textStatus, errorThrown) {
+                        if(textStatus == 'timeout' || textStatus == 'error'){
+                            document.querySelector('#TimeoutSup').open();
+                        }
+                    },
+                    success: function (data) {
+                        $("#loaderActivePopup").hide();
+                        var csvContent = '';
+                        var headers = ['SL.No', 'Name', 'Email','Contact', 'Role', 'Status'];
+                        csvContent += headers.join(',') + '\n';
+                         for (var i = 0; i < data[0].length; i++) {
+                            self.StaffDet.push({'no': i+1,'id': data[0][i][0],'name' : data[0][i][2] + " " + data[0][i][3], 'email': data[0][i][11],'contact': data[0][i][15] + data[0][i][12]  });
+                            var rowData = [i+1, data[0][i][2] + " " +  data[0][i][3],  data[0][i][11],  data[0][i][15]+ data[0][i][12]] ;
+                            csvContent += rowData.join(',') + '\n';
+                    }
+                    var blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+                    var today = new Date();
+                    var fileName = 'Pending_Staff_' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '.csv';
+                    self.blob(blob);
+                    self.fileName(fileName);
+                   
                     self.StaffDet.valueHasMutated();
                     return self; 
                 }
@@ -157,7 +268,23 @@ function (oj,ko,$, app, ojconverterutils_i18n_1, ArrayDataProvider,  ojknockout_
             }
             self.dataProvider = new ArrayDataProvider(this.StaffDet, { keyAttributes: "id"});
 
-
+            self.downloadData = ()=>{
+                if(self.blob() != undefined && self.fileName() != undefined){
+                    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+                        // For Internet Explorer
+                        window.navigator.msSaveOrOpenBlob(self.blob(), self.fileName());
+                    } else {
+                        // For modern browsers
+                        var link = document.createElement('a');
+                        link.href = window.URL.createObjectURL(self.blob());
+                        link.download = self.fileName();
+                        link.style.display = 'none';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }
+                }
+            }
            
         }
     }
