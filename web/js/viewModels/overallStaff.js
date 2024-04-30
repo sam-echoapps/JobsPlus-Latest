8358544e58f5a7d9680f8c88ece18a6b3f84d367
sam-echoapps <sam@echoapps360.com>
2024-04-30 18:37:14 +0530

    Overall Staff Profile Download Commit

--- web/js/viewModels/overallStaff.js
+++ web/js/viewModels/overallStaff.js
@@ -120,6 +120,26 @@ function (oj,ko,$, app, ojconverterutils_i18n_1, ArrayDataProvider, PagingDataPr
             self.flag = ko.observable('0');
 
 
+            self.name = ko.observable();
+            self.jobRoleStaff = ko.observable();
+            self.gender = ko.observable();
+            self.profilePhoto = ko.observable();
+            self.dbsNumber = ko.observable();
+            self.dbsExpiryDate = ko.observable();  
+            self.movingFile_expiry_date = ko.observable();
+            self.safeguarding_expiry_date = ko.observable();
+            self.health_expiry_date = ko.observable();
+            self.food_expiry_date = ko.observable();
+            self.support_expiry_date = ko.observable();  
+            self.coshh_expiry_date = ko.observable();
+            self.safety_expiry_date = ko.observable();
+            self.behaviour_expiry_date = ko.observable();
+            self.epilepsy_expiry_date = ko.observable();
+            self.act_expiry_date = ko.observable();
+            self.prevention_expiry_date = ko.observable();
+            self.disability_expiry_date = ko.observable();
+            self.care_expiry_date = ko.observable();
+
             var routerLength = context.parentRouter._routes.length;
             if(routerLength!=17){
                 location.reload();
@@ -1590,6 +1610,142 @@ self.getStaffWorkFilterInfoClear = function (event,data) {
 }; 
 
 
+self.profileView = function (event,data) {
+    var clickedRowId = sessionStorage.getItem("staffId")
+    console.log(clickedRowId)
+    document.querySelector('#openUserProfileInfo').open();
+    var BaseURL = sessionStorage.getItem("BaseURL")
+    $.ajax({
+        url: BaseURL+ "/jpStaffProfileInfoGet",
+        type: 'POST',
+        data: JSON.stringify({
+            rowId : clickedRowId,
+        }),
+        dataType: 'json',
+        timeout: sessionStorage.getItem("timeInetrval"),
+        context: self,
+        error: function (xhr, textStatus, errorThrown) {
+            if(textStatus == 'timeout'){
+                document.querySelector('#openUserProfileInfo').close();
+                document.querySelector('#Timeout').open();
+            }
+        },
+        success: function (result) {
+          console.log(result)
+           data = JSON.parse(result[0]);
+           console.log(data)
+           var data1 = JSON.parse(result[1]);
+           if(data1 == null){
+               self.movingFile_expiry_date('N/A')
+           }else{
+               self.movingFile_expiry_date(data1)
+           }
+           var data2 = JSON.parse(result[2]);
+           if(data2 == null){
+               self.safeguarding_expiry_date('N/A')
+           }else{
+               self.safeguarding_expiry_date(data2)
+           }
+           var data3 = JSON.parse(result[3]);
+           if(data3 == null){
+               self.health_expiry_date('N/A')
+           }else{
+               self.health_expiry_date(data3)
+           }
+           var data4 = JSON.parse(result[4]);
+           if(data4 == null){
+               self.food_expiry_date('N/A')
+           }else{
+               self.food_expiry_date(data4)
+           }
+           var data5 = JSON.parse(result[5]);
+           if(data5 == null){
+               self.support_expiry_date('N/A')
+           }else{
+               self.support_expiry_date(data5)
+           }
+           var data6 = JSON.parse(result[6]);
+           if(data6 == null){
+               self.coshh_expiry_date('N/A')
+           }else{
+               self.coshh_expiry_date(data6)
+           }
+           var data7 = JSON.parse(result[7]);
+           if(data7 == null){
+               self.safety_expiry_date('N/A')
+           }else{
+               self.safety_expiry_date(data7)
+           }
+           var data8 = JSON.parse(result[8]);
+           if(data8 == null){
+               self.behaviour_expiry_date('N/A')
+           }else{
+               self.behaviour_expiry_date(data8)
+           }
+           var data9 = JSON.parse(result[9]);
+           if(data9 == null){
+               self.epilepsy_expiry_date('N/A')
+           }else{
+               self.epilepsy_expiry_date(data9)
+           }
+           var data10 = JSON.parse(result[10]);
+           if(data10 == null){
+               self.act_expiry_date('N/A')
+           }else{
+               self.act_expiry_date(data10)
+           }
+           var data11 = JSON.parse(result[11]);
+           if(data11 == null){
+               self.prevention_expiry_date('N/A')
+           }else{
+               self.prevention_expiry_date(data11)
+           }
+           var data12 = JSON.parse(result[12]);
+           if(data12 == null){
+               self.disability_expiry_date('N/A')
+           }else{
+               self.disability_expiry_date(data12)
+           }
+           var data13 = JSON.parse(result[13]);
+           if(data13 == null){
+               self.care_expiry_date('N/A')
+           }else{
+               self.care_expiry_date(data13)
+           }
+           self.name(data[0] + " " + data[1] + " " + data[2])
+           self.jobRoleStaff(data[3])
+           self.gender(data[4])
+           console.log(result[14])
+           var container = document.getElementById("pic-print");
+           var imgElement = document.createElement("img");
+           imgElement.src = 'data:image/jpeg;base64,' + result[14];
+           imgElement.width = 80; // Remove the quotes
+           imgElement.height = 80; // Remove the quotes
+           container.appendChild(imgElement);
+           self.profilePhoto('data:image/jpeg;base64,'+result[14])
+           if(data[6] == null){
+           self.dbsNumber('N/A')
+           }else{
+               self.dbsNumber(data[6])
+           }
+           if(data[7] == null){
+               self.dbsExpiryDate('N/A')
+               }else{
+                   self.dbsExpiryDate(data[7])
+               }
+        }
+    })  
+
+}
+
+self.downloadProfile = function (event,data) {
+    //document.title = "Print page title";
+    var printContents = document.getElementById('DownloadProfile').innerHTML;
+    document.body.innerHTML = "<html><head><title></title></head><body>" + printContents + "</body>";
+    window.print(); 
+
+    location.reload()
+}
         
 
 
