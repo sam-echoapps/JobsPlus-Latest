eedd5d135abb4a0a70ad57de3eb0b579eebb9183
sam-echoapps <sam@echoapps360.com>
2024-07-17 15:05:11 +0530

    Timeout Signin Change Commit

--- web/js/viewModels/signin.js
+++ web/js/viewModels/signin.js
@@ -41,8 +41,8 @@ define(['ojs/ojcore', 'knockout', 'appController', 'appUtils',
                         }
                         var key = "."+generateString(8);
                         $.ajax({
-                            //url: "/jobPlusLogin", 
-                            url: "http://169.197.183.168:8090/jobPlusLogin",
+                            url: "/jobPlusLogin", 
+                            //url: "http://169.197.183.168:8090/jobPlusLogin",
                             type: 'POST',
                             data: JSON.stringify({
                                 user: self.OnePlaceuserName(),
@@ -50,7 +50,8 @@ define(['ojs/ojcore', 'knockout', 'appController', 'appUtils',
                                 onepsuid :  key
                             }),
                             dataType: 'json',
-                            timeout: sessionStorage.getItem("timeInetrval"),
+                            //timeout: sessionStorage.getItem("timeInetrval"),
+                            timeout: 1000 * 30,
                             context: self,
                             error: function (xhr, textStatus, errorThrown) {
                                 if(textStatus == 'timeout' || textStatus == 'error'){
@@ -60,8 +61,8 @@ define(['ojs/ojcore', 'knockout', 'appController', 'appUtils',
                             },
                             success: function (data) {
                                 if (data[1]== 'Y') {
-                                    //sessionStorage.setItem("BaseURL", "");
-                                    sessionStorage.setItem("BaseURL", "http://169.197.183.168:8090");
+                                    sessionStorage.setItem("BaseURL", "");
+                                    //sessionStorage.setItem("BaseURL", "http://169.197.183.168:8090");
                                     sessionStorage.setItem("userId", data[2]);
                                     sessionStorage.setItem("userName", data[3]);
                                     sessionStorage.setItem("userRole", data[4]);
